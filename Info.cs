6fd60d6f67e475a625dfeae34b4d87876f05092b
Leon Ask <leon.ask@outlook.com>
2025-01-20 08:08:32 +0100

    init

---
+++ Info.cs
@@ -0,0 +1,100 @@
+﻿using System.Management;
+
+namespace Myco;
+
+internal record CpuInfo(
+    string Name,
+    uint NumberOfCores,
+    uint NumberOfLogicalProcessors,
+    uint MaxClockSpeed,
+    uint CurrentClockSpeed,
+    uint L2CacheSize,
+    uint L3CacheSize,
+    string SocketDesignation
+);
+
+internal record GpuInfo(
+    string Name
+);
+
+internal record OsInfo(string Caption, string Version, string OSArchitecture);
+
+internal record MemoryInfo(ulong Capacity, string Manufacturer, string PartNumber, uint Speed);
+
+internal record DriveInfo(string Model, ulong Size);
+
+internal static class Info
+{
+    internal static CpuInfo GetCpuInfo()
+    {
+        var searcher = new ManagementObjectSearcher("select * from Win32_Processor");
+        var obj = searcher.Get().Cast<ManagementObject>().First();
+        var cpu = new CpuInfo(
+            (string)obj["Name"],
+            (uint)obj["NumberOfCores"],
+            (uint)obj["NumberOfLogicalProcessors"],
+            (uint)obj["MaxClockSpeed"],
+            (uint)obj["CurrentClockSpeed"],
+            (uint)obj["L2CacheSize"],
+            (uint)obj["L3CacheSize"],
+            (string)obj["SocketDesignation"]
+        );
+        return cpu;
+    }
+
+    internal static GpuInfo GetGpuInfo()
+    {
+        var searcher = new ManagementObjectSearcher("select * from Win32_VideoController");
+        var obj = searcher.Get().Cast<ManagementObject>().First();
+        var gpu = new GpuInfo(
+            (string) obj["Name"]
+        );
+        return gpu;
+    }
+
+    internal static OsInfo GetOsInfo()
+    {
+        var searcher = new ManagementObjectSearcher("select * from Win32_OperatingSystem");
+        var obj = searcher.Get().Cast<ManagementObject>().First();
+        var os = new OsInfo(
+            (string) obj["Caption"],
+            (string) obj["Version"],
+            (string) obj["OSArchitecture"]
+        );
+        return os;
+    }
+
+    internal static List<MemoryInfo> GetMemoryInfo()
+    {
+        var searcher = new ManagementObjectSearcher("select * from Win32_PhysicalMemory");
+        var memories = new List<MemoryInfo>();
+        foreach (var obj in searcher.Get().Cast<ManagementObject>())
+        {
+            var memory = new MemoryInfo(
+                (ulong)(obj["Capacity"]),
+                (string)obj["Manufacturer"],
+                (string) obj["PartNumber"],
+                (uint) obj["Speed"]
+            );
+            memories.Add(memory);
+        }
+
+        return memories;
+    }
+
+    internal static List<DriveInfo> GetDriveInfo()
+    {
+        var searcher = new ManagementObjectSearcher("select * from Win32_DiskDrive");
+        var drives = new List<DriveInfo>();
+        foreach (var obj in searcher.Get().Cast<ManagementObject>())
+        {
+            var drive = new DriveInfo(
+                (string)obj["Model"],
+                (ulong) obj["Size"]
+            );
+            drives.Add(drive);
+        }
+
+        return drives;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Spectre.Console;
+using Myco;
+
+var cpu = Info.GetCpuInfo();
+var gpu = Info.GetGpuInfo();
+var os = Info.GetOsInfo();
+var memories = Info.GetMemoryInfo();
+
+
+var layout = new Layout("root")
+    .SplitColumns(
+        new Layout("left"),
+        new Layout("right")
+    );
+
+
+var totalMemoryCapacity = memories.Sum(x => (decimal) x.Capacity);
+var memoryInGb = totalMemoryCapacity / 1024 / 1024 / 1024;
+var osString = $"{os.Caption} {os.Version} {os.OSArchitecture}";
+var cpuClockSpeed = (decimal) cpu.MaxClockSpeed / 1000;
+
+var cpuRender =
+    new Grid()
+        .AddColumn()
+        .AddColumn()
+        .AddRow("[white]OS[/]", $"[white]{osString}[/]")
+        .AddRow("[blue]CPU[/]", $"[blue]{cpu.Name}[/]")
+        .AddRow("[blue]Cores[/]", $"[blue]{cpu.NumberOfCores}[/]")
+        .AddRow("[blue]Logical Processors[/]", $"[blue]{cpu.NumberOfLogicalProcessors}[/]")
+        .AddRow("[blue]Base Speed[/]", $"[blue]{cpuClockSpeed} GHz[/]")
+        .AddRow("[green]GPU[/]", $"[green]{gpu.Name}[/]")
+        .AddRow("[grey]Memory[/]", $"[grey]{memories.First().Manufacturer}[/]")
+        .AddRow("[grey]Capacity[/]", $"[grey]{memoryInGb} GB[/]")
+        .AddRow("[grey]Speed[/]", $"[grey]{memories.First().Speed} MHz[/]")
+    ;
+
+var combinedPanel = new Panel(
+    cpuRender
+).Expand();
+
+layout["right"].Update(combinedPanel);
+
+AnsiConsole.Write(layout);
